0784ff9c807e473d7683acd0a16d43b2a61ae0dc
Komang Sri Yanisa Putri <sriyanisa03@gmail.com>
2026-08-05 13:32:01 +0800

    Update CACHE_NAME to version 3.4.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pesona-geo-v3.3.0'; // Versi sudah dinaikkan untuk mereset cache
+const CACHE_NAME = 'pesona-geo-v3.4.0'; // Versi sudah dinaikkan untuk mereset cache
 const urlsToCache = [
   '/gempita_ht/',
   '/gempita_ht/index.html',
